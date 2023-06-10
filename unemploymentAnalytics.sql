d4082c1b5a22b4ec997ea342861b9333e0949105
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2023-06-09 17:47:53 -0700

    Add files via upload

---
+++ unemploymentAnalytics.sql
@@ -0,0 +1,17 @@
+
+SELECT DISTINCT "month" , province, year, 
+CAST(AVG(employment_rate) OVER(PARTITION BY Province,YEAR,MONTH ORDER BY Year) AS DECIMAL(10,2)) AS avg_employment_rate_per_year_and_month,
+CAST(AVG(unemployment_rate) OVER(PARTITION BY Province,YEAR,MONTH ORDER BY Year) AS DECIMAL(10,2)) AS avg_unemployment_rate_per_year_and_month,
+CAST(AVG(employment_rate) OVER(PARTITION BY Province,YEAR ORDER BY Year) AS DECIMAL(10,2)) AS avg_employment_rate_per_year,
+CAST(AVG(unemployment_rate) OVER(PARTITION BY Province,YEAR ORDER BY Year) AS DECIMAL(10,2)) AS avg_unemployment_rate_per_year,
+CAST(AVG(employment_rate) OVER(PARTITION BY Province ORDER BY Province) AS DECIMAL(10,2)) AS avg_employment_rate_per_province,
+CAST(AVG(unemployment_rate) OVER(PARTITION BY Province ORDER BY Province) AS DECIMAL(10,2)) AS avg_unemployment_rate_per_province
+INTO unemploymentCanadaAnalytics
+FROM Unemploymentcanada
+ORDER BY PROVINCE, YEAR
+;
+
+SELECT * FROM unemploymentCanadaAnalytics
+
+SELECT *
+FROM UnemploymentCanada
