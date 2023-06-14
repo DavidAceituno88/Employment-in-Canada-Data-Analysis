c251f3ddde754e954227942b4404a20355d9a042
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2023-06-13 17:18:50 -0700

    Create FinalTable.sql
    This is the final table script
    Where i join all the other tables to the main UnemploymentCanada table

---
+++ FinalTable.sql
@@ -0,0 +1,16 @@
+SELECT u.Province, u.year, u.month, u.fromAge, u.toAge, u.employment_num, u.unemployment_num,
+u.fullTime_Emp, u.part_time_emp, u.labour_force, u.population_total,
+u.employment_rate, u.unemployment_rate, u.participation_rate,
+uc.avg_employment_rate_per_year,uc.avg_unemployment_rate_per_year,
+uc.avg_employment_rate_per_year_and_month, uc.avg_unemployment_rate_per_year_and_month,
+uc.avg_employment_rate_per_province,uc.avg_unemployment_rate_per_province,
+p.name, p.political_party
+INTO employmentCanadaAnalysis
+FROM UnemploymentCanada u
+JOIN UnemploymentCanadaAnalytics uc ON u.province = uc.province AND u.year = uc.year AND u.month = uc.month
+JOIN primeMinistersClean p ON u.year BETWEEN p.start_mandate AND p.end_mandate -1 --Substract a year to avoid OVERLAPING BETWEEN PRIME MINISTERS 
+ORDER BY u.year
+
+
+
+SELECT * FROM employmentCanadaAnalysis
