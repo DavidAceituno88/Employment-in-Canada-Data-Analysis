7b5c6d95cabae17a997de1d9b07933001e22f15e
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2023-06-23 17:37:40 -0700

    Update primeMinistearsCleaning.sql

--- WebScraper/primeMinistearsCleaning.sql
+++ WebScraper/primeMinistearsCleaning.sql
@@ -1,14 +1,14 @@
  /*Clean Data*/
  SELECT column1 AS Name, 
-		--We get the year from the original date format wich was dd-mm-yy--
+		--I got the year from the original date format wich was dd-mm-yy--
 		RIGHT(column2,2) AS Start_mandate, 
 		/*Since the last minister is still elected there was a string in the End_mandate column, 
-		so we need to replace any string with the actual year ex "Still elected"  in the end_mandate column*/
+		so i need to replace any string with the actual year ex "Still elected"  in the end_mandate column*/
 		CASE WHEN column3 NOT LIKE '[a-z]%' 
 		THEN RIGHT(column3,2) ELSE 2023 END AS End_mandate, column5 AS Political_party
   INTO primeMinisters
   FROM primeMinistersTbl
-  /*We get rid of the ministers from the 1800s, get rid of those weird duplicates with text related to the prime minister
+  /*I got rid of the ministers from the 1800s, get rid of those weird duplicates with text related to the prime minister
   and lastly we get rid of a weird Pierre Trudeao duplicate column*/
   WHERE column2 NOT LIKE '%18%' AND column2 LIKE '[0-9]%' AND column2 NOT LIKE '% [A-Z]%'
 
