533c6b31e1ad4d4ec33dedeab117bdc073a7efc1
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2023-06-28 17:44:25 -0700

    Update unemploymentAnalytics.sql

--- unemploymentAnalytics.sql
+++ unemploymentAnalytics.sql
@@ -8,10 +8,6 @@ CAST(AVG(employment_rate) OVER(PARTITION BY Province ORDER BY Province) AS DECIM
 CAST(AVG(unemployment_rate) OVER(PARTITION BY Province ORDER BY Province) AS DECIMAL(10,2)) AS avg_unemployment_rate_per_province
 INTO unemploymentCanadaAnalytics
 FROM Unemploymentcanada
-ORDER BY PROVINCE, YEAR
-;
+ORDER BY PROVINCE, YEAR;
 
 SELECT * FROM unemploymentCanadaAnalytics
-
-SELECT *
-FROM UnemploymentCanada
